68c4bb4f43caff855c33ddc92a186cbdd9c846b7
Yitz385 <isaac385@hotmail.com>
2024-10-11 20:14:09 -0600

    Proyecto ZOO/Isaac J. G./Cesar S. H.

---
+++ Proyecto/Empleados/empleados.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+nombre: str
+apellidos: str
+fecha_ingreso_como_trabajador: datetime
+fecha_nacimiento: datetime
+curp: str
+rfc: str
+salario: float
+horario: int
+rol: "(Veterinario, Guia, Mantenimiento, Administración)"
---
+++ Proyecto/Visitantes/visitantes.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+nombre: str
+apellidos: str
+fecha_nacimiento: datetime
+curp: str
+numero_visitas = (0)
+fecha_registro : datetime
